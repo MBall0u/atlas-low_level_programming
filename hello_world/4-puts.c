d808933d172e89dbbc9150965afb9faeb9e9acee
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-18 05:19:55 +0000

    fixed betty errors

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-/* Hello, puts */
-
+/*
+* main - entry point
+*
+* Description: Hello, puts
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
