6f1f3280ba7ae80382a5f0283bd93058d89f0cdf
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-19 19:03:55 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - I sometimes suffer from insomnia. And when I can't fall asleep,
+* I play what I call the alphabet game
+*
+* Description - Write a program that prints the alphabet in lowercase,
+* followed by a new line
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
