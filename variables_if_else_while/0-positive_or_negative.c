5ff2ed8cc86d277ae09970f9f07cc53b5f75c3a0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-19 16:22:32 +0000

    betty correction two

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 /**
 * main - Positive anything is better than negative nothing
 *
-* Description - This program will assign a random number to the variable n each
-* time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative
+* Description - This program will assign a random number to the variable n
+* each time it is executed. Complete the source code in order to print
+* whether the number stored in the variable n is positive or negative
 *
 * Return: Always 0 (success)
 */
