3c8cdd78156a4a7b77169d769d9b478ac788f9fc
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-20 23:40:58 +0000

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -19,9 +19,12 @@ int main(void)
 
 void print_alphabet()
 {
-	for (char c  =  'a' ; c <= 'z' ; c++)
+	char c = 'a';
+
+	while(c <= 'z')
 	{
 		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 	return (0);
