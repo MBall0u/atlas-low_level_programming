a96e36a002c4f11fd8d280e1737c884d59943915
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-20 02:23:06 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Patience, persistence and perspiration make an unbeatable
+* combination for success
+*
+* Description - Write a program that prints all possible combinations
+* of single-digit numbers
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
