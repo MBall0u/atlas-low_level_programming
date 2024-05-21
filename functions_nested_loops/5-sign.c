14ab0ee05497a587e7396d6a8557eaf07b3ed2e4
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 04:06:40 +0000

    ksjnfkn

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* print_sign - prints the sign of a number
+*
+* Description - if greater, less than, or zero
+*
+* @n: aidnfjanfn
+*
+* Return: Always 0 (success)
+*/
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
