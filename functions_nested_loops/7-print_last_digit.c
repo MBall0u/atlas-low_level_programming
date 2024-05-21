428143deb6d13d84afaa5c663798301e92095db9
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 19:30:34 +0000

    kjsdbfkb

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - sjhdfjhsfhj
+*
+* Description - ksjbdfkbfjks
+*
+* @i: ksdfbds
+*
+* Return: Always 0 (success)
+*/
+int print_last_digit(int i)
+{
+	int res;
+
+	res = i % 10;
+	return (res);
+}
