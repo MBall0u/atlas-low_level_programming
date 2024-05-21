999bce52027f898bcc428eef927803c8934fcdf5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 00:53:49 +0000

    kjsnf

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* main - 10 x alphabet
+*
+* Description - Write a function that prints 10 times the alphabet,
+* in lowercase, followed by a new line
+*
+* Return: Always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+	int a = 1;
+	char c = 'a';
+
+	while (a <= 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
