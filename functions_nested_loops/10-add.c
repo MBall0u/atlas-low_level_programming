b4c0afb7e8a377af091619a30a16f1a0a87ede2e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 04:57:45 +0000

    ksbfjhsv

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+\#include <unistd.h>
+#include "main.h"
+/**
+* add - add kjasfjk
+*
+* Description: ksnfkjsbdf
+*
+* @a: kdjfnskndf
+*
+* @b: ksbfjsdb
+*
+* Return: always 0 (success)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* _abs - kjdfsnfn
+*
+* Description - fakjsdfbasdf
+*
+* Return: Always 0 (success)
+*/
+int _abs(int)
+{
+	if (
+}
