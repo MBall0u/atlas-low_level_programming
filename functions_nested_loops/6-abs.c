f51e96c0451ce6695203b13734f6c1dfbf67a092
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 19:14:15 +0000

    sbf

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,22 @@
 #include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 /**
 * _abs - kjdfsnfn
 *
 * Description - fakjsdfbasdf
 *
+* @i: skjfksb
+*
 * Return: Always 0 (success)
 */
-int _abs(int)
+int _abs(int i)
 {
-	if (
+	int res;
+
+	if (i < 0)
+		res = i * -1;
+	else if (i >= 0)
+		res = i;
+	return (res);
 }
