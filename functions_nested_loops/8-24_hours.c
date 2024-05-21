2743112c1b0ac3316fc7064f7a6907e505474dd6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-21 20:20:10 +0000

    ksjbdf

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* jack_bauer - sjdbfjsbf
+*
+* Description - kjsnfkjbsd
+*
+* Return: Always 0 (success)
+*/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0 ; h <= 23 ; h++)
+	{
+		int th = h / 10;
+
+		_putchar(th + '0');
+		th = h % 10;
+		_putchar(th + '0');
+
+		for (m = 0 ; m <= 59 ; m++)
+		{
+			int tm = m / 10;
+
+			_putchar(tm + '0');
+			tm = m % 10;
+			_putchar(tm + '0');
+		}
+		_putchar(':');
+	}
+	return (0);
+}
