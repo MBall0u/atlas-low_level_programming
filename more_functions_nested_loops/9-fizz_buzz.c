ae0337857167abfec9833c9c4f6e4ab92e45d2f7
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-22 23:54:02 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - FizzBuzz
+*
+* Description - prints fizzbuzz
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	return (0);
+}
