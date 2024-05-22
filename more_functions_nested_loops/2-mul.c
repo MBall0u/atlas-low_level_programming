961a07563ce009e3bd19a8efdf7ae6b0eeefa645
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-22 20:39:28 +0000

    Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* mul - multipies the two variables
+*
+* Description - a function that multiplies two integers
+*
+* @a - variable a
+*
+* @b - variable b
+*
+* Return: Always 0 (success)
+*/
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
