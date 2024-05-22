8aa6f449c220e1b6e15ba7e9419d260917d997dd
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-22 20:47:28 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* print_numbers - The numbers speak for themselves
+*
+* Description - Write a function that prints the numbers, from 0 to 9, followed
+* by a new line
+*
+* Return: Always 0 (success)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n");
+	return;
+}
