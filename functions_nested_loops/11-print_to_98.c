5c9695eec56a4cb5c27c024643c6cf1b27b5c8d0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-22 03:45:13 +0000

    ksjndfk

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - sbjfbsjhdbf
+*
+* Description - kjsnfkb
+*
+* @n: kjshkhs
+*
+* Return: Always 0 (success)
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else (n == 0)
+	{
+		printf("%d", n);
+	}
+}
