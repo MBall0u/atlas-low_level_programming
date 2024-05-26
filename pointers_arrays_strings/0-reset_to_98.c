54276dd308004e91c08351bfa303908c36b105d5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-26 19:46:18 +0000

    98 Battery st

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* reset_to_98 - 98 Battery st.
+*
+* Description - Write a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98
+*
+* @n: variable being set to 98
+*
+* Return: Always 0 (success)
+*/
+void reset_to_98(int *n)
+{
+	int i = 98;
+
+	n = &n;
+
+	return (0);
+}
