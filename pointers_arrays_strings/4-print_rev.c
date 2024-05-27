ffc8091a8835742ad66c69efb09c2644a320c60f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-27 05:28:53 +0000

    akjsbdfkj

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* print_rev - I can only go one way. I've not got a reverse gear
+* Description - Write a function that prints a string, in reverse, followed
+* by a new line
+* @s: variable
+* Return: Always 0 (success)
+*/
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	while (c >= 0)
+	{
+		_putchar(s[c - 1]);
+		c--;
+	}
+	_putchar('\n');
+}
