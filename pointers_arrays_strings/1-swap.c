3eff10256f854d6750ac20d148641255a3640c32
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-27 04:03:52 +0000

    Don't swap horses in crossing a stream

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* swap_int - Don't swap horses in crossing a stream
+* Description - Write a function that swaps the values of two integers
+* @a: switching with variable b
+* @b: switching with variable a
+* Return: Always 0 (success)
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
