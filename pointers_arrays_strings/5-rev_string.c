a0a986eb28ddaab46f46379fde71e4c24c03070a
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-27 19:04:49 +0000

    fixed comilation issue i think

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,14 @@
 void rev_string(char *s)
 {
 	int c = 0;
+	char t;
+	int i = 0;
+	int e = c - 1;
 
 	while (s[c] != '\0')
 	{
 		c++;
 	}
-
-	char t;
-	int i = 0;
-	int e = c-1;
 
 	while (i < e)
 	{
