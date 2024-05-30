2527c39aa4f98cc5685ae2b60691e71c65ce0594
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-05-30 10:34:50 +0000

    added all of the prototypes for the project 'C - More pointers, arrays and strings' into my main.h file as well as started the first task strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+* _strcat - strcat
+* Description - Write a function that concatenates two strings
+* @dest: the first string
+* @src: the second string that is being added to the back of dest
+* Return: Always 0 (success)
+*/
+char *_strcat(char *dest, char *src)
+{
+}
