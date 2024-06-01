d06272e3d8534b8152e0fdf893d9879ea85a03ac
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-01 18:57:01 +0000

    ksjdfkjb

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,17 +5,36 @@
 * @dest: the destination for the string copied from src
 * @src: the string that is going to be copied to dest
 * @n: the number of characters that will be copied from src to dest
-* Returns: Always 0 (success)
+* Return: Always 0 (success)
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *temp = dest;
-	int count = 0;
+	char *count = src;
+	int tc = 0;
+	int cc = 0;
 
-	while (*src != '\0' && count < n)
+	while (*count != '\0')
+	{
+		*count++;
+		cc++;
+	}
+
+	if (n > cc)
+	{
+		while (*src != '\0' && tc < n)
+		{
+			*temp++ = *src++;
+			tc++;
+		}
+
+		*temp = '\0';
+	}
+
+	while (*src != '\0' && tc < n)
 	{
 		*temp++ = *src++;
-		count++;
+		tc++;
 	}
 
 	return (dest);
