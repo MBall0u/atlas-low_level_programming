f00961e75f8eb355b5de9bf0ed575e071ef3e663
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-01 15:52:04 +0000

    kjsndkfbs

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,4 +10,19 @@
 */
 char *_strcat(char *dest, char *src)
 {
+	char *temp = dest;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	while (*src != '\0')
+	{
+		*temp++ = *src++;
+	}
+
+	*temp = '\0';
+
+	return dest;
 }
