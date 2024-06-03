0a5609dc0e6fc5c56da925c590cda78ade25677f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-03 23:07:09 +0000

    ksjdbfjhb

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strpbrk - sbfjbskjbf
+* Description - ksbjfdb
+* @s: kjsndfknsd
+* @accept: kjsbjbdf
+* Return: Always 0 (success)
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s != '\0')
+	{
+		for (a = 0; accept[a] != '\0'; a++)
+		{
+			if (*s == accept[b])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
