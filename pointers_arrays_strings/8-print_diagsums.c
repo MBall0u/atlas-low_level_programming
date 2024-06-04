5fb6ef3af3893a3dd63a5e98d2c8e205449429cd
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-04 02:20:39 +0000

    skdfksjb

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - skdfbskjfb
+* Description - ksndksdkj
+* @a: snfkjndskjfn
+* @size: sdbfksjdbfkj
+* Return: Always 0 (success)
+*/
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int b = 0;
+	int c = size - 1;
+
+	for (b = 0; b < size; b++)
+	{
+		sum1 += a[b][b];
+	}
+	for (b = 0; b < size; b++)
+	{
+		sum2 += a[b][c];
+		c--;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
