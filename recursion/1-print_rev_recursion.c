a8d7c24576b3630d0c87adf13ab13529c0331cde
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-07 16:05:00 +0000

    push with code for task 1 project 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,18 @@
 #include "main.h"
 /**
+* _print_rev_recursion - prints the provided string in reverse
+* Decription - see above line
+* @s: the provided string
+* Return: Always 0 (success)
 */
 void _print_rev_recursion(char *s)
 {
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(&s[i + 1]);
+	_putchar(s[i]);
 }
