4b5d9a4cbd75b3ef70bbdc6f0ab3ea726397e871
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-07 05:11:59 +0000

    so much python tutor time smh

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	_putchar(s[i]);
-
 	if (s[i] == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	_puts_recursion(s[i + 1]);
+	_putchar(s[i]);
+	_puts_recursion(&s[i + 1]);
 }
