d1882942b3378b285adc9ef5214e2ba811571af5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-07 17:36:57 +0000

    task 3 on project 1, this was easy peasy(cool guy emoji with sunglasses on)

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* factorial - does factorial multiplication recursively?
+* Description - see above comment
+* @n: the starting factorial
+* Return: Always 0 (success)
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n-1));
+}
