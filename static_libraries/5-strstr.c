8caf38d0900b87f00657f931b57fd6514d3d0c7a
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-09 21:49:03 +0000

    task 0 project 2, I made all of my object files and my library

--- static_libraries/5-strstr.c
+++ static_libraries/5-strstr.c
@@ -1,14 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 /**
+* _strstr - compares two strings and if the substring needle is found
+* in haystack then it returns haystack, if it does not
+* find the needle in the haystack then return null
+* Description - see above comment
+* @haystack: the inputted sting with multiple words
+* @needle: the substring that we are searching for in the haystack
+* Return: Always 0 (success)
 */
 char *_strstr(char *haystack, char *needle)
 {
-}
+	int i = 0;
 
-/* for the string haystack it needs to iterate character by character
-checking each one against needle and once it matches the first character
-of needle with a character in haystack it checks the next characters
-against needle till a null byte is met in needle meaning the entire string
-has been found
-*/
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i == 0)
+	{
+		return (haystack);
+	}
+
+	while (*haystack != '\0')
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*b == *a)
+		{
+			a++;
+			b++;
+
+			if (*b == '\0')
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+	return (0);
+}
