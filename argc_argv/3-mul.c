fd34a2e5a0e7c7cbc214d784f1e82e09d1d189dc
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-10 22:05:15 +0000

    task 3 project 3, this will multiply two numbers into each other

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - this is going to take two number and multiply them
+* Description - see comment above
+* @argc: the arg count
+* @argv: the arg vectors
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int result = 1, i;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i <= argc; i++)
+	{
+		result *= atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
