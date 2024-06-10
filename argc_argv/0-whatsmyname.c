aac2ae2a6242d9c2bcc90bf3a3f980da38938fb8
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-10 21:34:11 +0000

    task 0 project 3, printing the first argument which is the name of the file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - will print the first arg pushed to the main function
+* Description - see above comment
+* @argc: the argument count
+* @argv: the argument vectors
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
