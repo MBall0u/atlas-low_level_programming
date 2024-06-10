8cbf2a508a6d156c1da0d3d94c6b0e2efe804c28
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-10 21:47:35 +0000

    task 1 project 3, will print the number of arguments passed through

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main- it will print the number of arguments passed to it
+* Description - see above comments
+* @argc: the number of arguments passed
+* @argv: the argument vectors
+* Return: 0 (success)
+*/
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
