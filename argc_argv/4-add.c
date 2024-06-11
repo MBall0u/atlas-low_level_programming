7b66d424ec2a7349ef870660994e5351daa612d0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-11 03:51:31 +0000

    task 4 project 3, testing testing one two

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - this is going to add an indescript argv's
+* Description - see above comment
+* @argc: argument count
+* @argv: argument vector
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int result = 0;
+	int i;
+
+	if (argc <= 2)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (i <= '0' || i >= '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		result += argv[i];
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
