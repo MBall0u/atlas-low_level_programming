4ae23aadcfe6a28ad681db4b6c959348ea39ac59
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-14 18:46:07 -0500

    task 0 project 1, wrote a function that creates an array and initializes a char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - this function creates an array of chars and
+ * initializes it with a specific char
+ * Description - see the above comment
+ * @size: the size of the array
+ * @c: the character that is being initialized in the array
+ * Return: Always 0 (success)
+*/
+char *create_array(unsigned int size, char c)
+{
+    int i;
+    char *ptr;
+
+    ptr = malloc(sizeof(char) * size + 1);
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < size; i++)
+    {
+        ptr[i] = c;
+    }
+    free(ptr);
+    return (ptr);
+}
