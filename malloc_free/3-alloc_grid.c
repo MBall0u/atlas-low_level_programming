9aceed49c714681fbe5f99d5c8910faea121d920
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-14 21:44:54 -0500

    task 3 project 1, hopefully this works

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2D array of ints
+ * Description - see above comment
+ * @width: the number of columns in the 2D array
+ * @height: the number of rows in the 2D array
+ * Return: always 0 (success)
+*/
+int **alloc_grid(int width, int height)
+{
+	int *grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int) * width * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	while (1)
+	{
+		for (i = 0; i < width; i++)
+		{
+			for (j = 0; j < height; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+		return (grid);
+	}
+	return (NULL);
+}
