af6591cfefc670cf414770644c773372f03b2e98
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-14 20:37:09 -0500

    task 2 project 1, think this will work unless betty doesn't like it

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - this fuction is going to combine two strings
+ * Description - see above comment
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0 (success)
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	int i, count = 0, j = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		count++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		count++;
+	}
+	new_str = malloc(sizeof(char) * count + 1);
+
+	while (1)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			new_str[j] = s1[i];
+			j++;
+		}
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			new_str[j] = s2[i];
+			j++;
+		}
+		j++;
+		new_str[j] = '\0';
+		return (new_str);
+	}
+	return (NULL);
+}
