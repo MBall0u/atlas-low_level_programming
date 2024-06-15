90c9a4c7fce84034c9bb5b810de8e74cc8baaec7
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-14 22:11:07 -0500

    hot fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,23 +10,24 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int *grid;
+	int **grid;
 	int i, j;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int) * width * height);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 	while (1)
 	{
-		for (i = 0; i < width; i++)
+		for (i = 0; i < height; i++)
 		{
-			for (j = 0; j < height; j++)
+			grid[i] = malloc(sizeof(int *) * width);
+			for (j = 0; j < width; j++)
 			{
 				grid[i][j] = 0;
 			}
