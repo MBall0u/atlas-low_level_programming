282bf830b02e678cfda41e3c033e32443afe4b2f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-16 19:54:13 -0500

    task 1 project 3, should be correct

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof *ptr * b);
+
+	if (ptr == NULL)
+	{
+
+	}
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory is being used for the project C - More malloc, free
