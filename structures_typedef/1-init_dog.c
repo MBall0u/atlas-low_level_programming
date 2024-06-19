089872054dcdb3eaab4042b7177fb8de80392e19
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 19:02:19 +0000

    BETTYYYYY and null case

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include "dog.h"
+/**
+* init_dog - initializes a variable of type struct dog
+* Description - see above comment
+* @d: pointer to structure
+* @name: dog name
+* @age: dog age
+* @owner: dogs owner
+* Return: Always 0 (success)
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
