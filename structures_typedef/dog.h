4de7ebd32c55324e47651b83ba7ee88f2be1a8bf
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 17:26:47 +0000

    BETTYYYYYYYYYYYYY

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,11 @@
+/**
+* struct dog - setting up a new structure
+* @name: dog name
+* @age: dog age
+* @owner: dogs owner
+*
+* Description: see above comments for a description
+*/
 #ifndef dog_h
 #define dog_h
 
