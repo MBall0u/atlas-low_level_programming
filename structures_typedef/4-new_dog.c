82b3a23ed1a286e9c1bd1f52c231e55f9fc5dfc1
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 17:08:15 -0500

    ksdbfjdsbf

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct new_dog;
+	
+	new_dog.name = name;
+	new_dog.age = age;
+	new_dog.owner = owner;
+	return (new_dog);
+}
