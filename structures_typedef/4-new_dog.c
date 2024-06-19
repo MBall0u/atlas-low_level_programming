2a1b313f9ebbc302c2d368c72684846a347adaff
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 17:15:52 -0500

    sdfbjsdb

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,9 +13,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct new_dog
 	{
-	char *name = name;
-	float age = age;
-	char *owner = owner;
+	char *name;
+	float age;
+	char *owner;
 	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
 	return (new_dog);
 }
