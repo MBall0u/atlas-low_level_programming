0337f52a635726795178394ace9a1182e0d88f15
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 17:42:41 -0500

    kjdfjh

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * 
- * 
- * 
- * 
- * 
- * 
+ * new_dog - creates a new dog
+ * Description - see above
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Result: Always 0 (success)
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *name_dup = name;
+	char *owner_dup = owner;
+
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = name_dup;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = owner_dup;
 	return (new_dog);
 }
