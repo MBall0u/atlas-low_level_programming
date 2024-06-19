494c4f5b01b00d25f56465f6afd7669997121944
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-19 16:48:55 -0500

    skdbfkjsdhf

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,21 +10,17 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
+		if (d->name == NULL || d->owner == null)
 		{
-			d->name = "(nil)";
+			if (d->name == NULL)
+			{
+				d->name = "(nil)";
+			}
+			if (d->owner == NULL)
+			{
+				d->owner = "(nil)";
+			}
 		}
-		else if (d->age == 0)
-		{
-			printf("Name: %s\n(nil)\nOwner: %s\n", d->name, d->owner);
-		}
-		else if (d->owner == NULL)
-		{
-			printf("Name: %s\nAge: %f\n(nil)\n", d->name, d->age);
-		}
-		else
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
