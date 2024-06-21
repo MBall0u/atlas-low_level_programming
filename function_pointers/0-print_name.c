638c3bccd941c60bb385389311b1095c92665496
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-21 13:53:28 +0000

    I got carried away and hadn't made the new .h file, the readme for the new directory, or even the new directory

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - a funtion pointer to other fuctions utilizing the name argument
+* Decription - see above
+* @name: variable being passed from the main functions to the fuctions this function is pointing to
+* @f: the fuction pointer to the fuction that is being called from the main function
+* Return: Always 0 (success)
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	char *new_name = name;
+
+	f(new_name);
+}
