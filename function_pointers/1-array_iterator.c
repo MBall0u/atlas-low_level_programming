7ecade01d16b01d2e31611ee1240527486d2ed45
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-21 15:02:29 +0000

    why am I understanding this now?

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* array_iterator - a function that executes a function given
+* as a parameter on each element of an array
+* Description - see above
+* @array: the given array
+* @size: the size of the array
+* @action: the pointer pointing to the given pointer funtion?
+* Return: Always 0 (success)
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int count;
+
+	for (count = 0; count < size; count++)
+	{
+		action(array[count]);
+	}
+}
