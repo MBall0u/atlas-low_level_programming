474dd90b8ae2b34ec1879a6ad6b2f6b12280e485
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-21 13:54:35 +0000

    made betty happy

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,8 +3,10 @@
 /**
 * print_name - a funtion pointer to other fuctions utilizing the name argument
 * Decription - see above
-* @name: variable being passed from the main functions to the fuctions this function is pointing to
-* @f: the fuction pointer to the fuction that is being called from the main function
+* @name: variable being passed from the main functions to the
+* fuctions this function is pointing to
+* @f: the fuction pointer to the fuction that is being called
+* from the main function
 * Return: Always 0 (success)
 */
 void print_name(char *name, void (*f)(char *))
