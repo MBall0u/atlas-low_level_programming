02b42215baa5b2370bcc7256e5babe116efc6572
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 11:20:05 -0500

    kjsnfk

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,25 +15,25 @@
 int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
-	const unsigned int i = 'argv[2]';
+	char *op = argv[2];
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (i < '%' || i > '/')
+	if (op != "%" || op != "/" || op != "*" || op != "+" || op != "-")
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((i == '/' || i == '%') && (num2 == 0))
+	if ((op == "/" || op == "%") && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", (*get_op_func(argv[2]))(num1, num2));
+	printf("%d\n", (get_op_func(op)(num1, num2)));
 
 	return (0);
 }
