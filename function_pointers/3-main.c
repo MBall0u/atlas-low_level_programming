ca7b9208bee9fdd962fe7558915edccf614f6056
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 10:42:34 -0500

    made my main calc function and i think my get op function is also good too so i am going to betty check run a test or two and then test

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - main calc function
+ * Description - this function will take three command line
+ * arguments and pass them to a pointer function that will
+ * determine what operator was passed through and then pass
+ * the arguments to another function that will do the operation
+ * and then return the result as an integer
+ * @argc: the argument count
+ * @argv: the array of arguments passed
+ * Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	char op = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ('op' < '%' || 'op' > '/')
+	{
+		printf("Error\n")
+		exit(99);
+	}
+	if (('op' == '/' || 'op' == '%') && (num2 == 0))
+	{
+		printf("Error\n")
+		exit(100);
+	}
+
+	printf("%d\n", get_op_function(op)(a, b));
+
+	return (0);
+}
