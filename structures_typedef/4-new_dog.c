99e422b74d870750bf07b99d8040a1cc16b344ca
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 15:13:34 -0500

    ksdbfjdsbf

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		free(new_dog.name);
+		free(new_dog->name);
 		free(new_dog->name);
 		free(new_dog->owner);
 		return (NULL);
