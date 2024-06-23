e3e059e1966f0e2446a2e325194f4bd162f4f486
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 12:27:39 -0500

    kjsnfksnd

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,12 +22,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (op != "%" || op != "/" || op != "*" || op != "+" || op != "-")
+	if (op != '%' || op != '/' || op != '*' || op != '+' || op != '-')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((op == "/" || op == "%") && (num2 == 0))
+	if ((op == '/' || op == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
