46109d4606ea6455e0445978b52d1004467a9911
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 15:55:27 +0000

    betty is appeased

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * get_op_function - 
- * Description - 
- * @s: 
+ * get_op_func - pointer function to the operator functions
+ * Description - this function is a pointer function that
+ * takes three arguments and passes them to a fuction
+ * that will do the given operation and return an integer
+ * @s: this will be an operator that is passed from the
+ * command line to the main function and then to this one
  * Return: Always 0 (success)
 */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
 
 	while (ops[i] != NULL)
 	{
