e465c11053cb68d4dae93f75e2102100ee4728f1
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 11:07:26 -0500

    corrected checks in main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,25 +15,25 @@
 int main(int argc, char *argv[])
 {
 	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
-	char *op = argv[2];
+	int i = 'argv[2]';
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (op < '%' || op > '/')
+	if (i < '%' || i > '/')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((op == '/' || op == '%') && (num2 == 0))
+	if ((i == '/' || i == '%') && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_function(op)(a, b));
+	printf("%d\n", get_op_func(argv[2])(num1, num2));
 
 	return (0);
 }
