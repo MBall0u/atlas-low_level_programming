78809b86248a4cd44f9b627cf153ebfabbefb241
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-23 14:43:00 -0500

    it is decided i will commit

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,14 +14,19 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
-	char op = argv[2][0];
+	int num1, num2;
+	char op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2][0];
+
 	if (op != '%' && op != '/' && op != '*' && op != '+' && op != '-')
 	{
 		printf("Error\n");
