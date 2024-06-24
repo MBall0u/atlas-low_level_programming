759936d89373f21a59e36c51e648b2a500b177cc
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-24 17:58:54 +0000

    iterating too much

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,14 +20,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator != NULL)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
 		{
 			printf("%s%d", separator, va_arg(ap, int));
 		}
 	}
 	if (separator == NULL)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
 		{
 			printf(" %d", va_arg(ap, int));
 		}
