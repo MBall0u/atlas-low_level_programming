9921b856126bc5e198b1dd3f0e46b389c1c7e91f
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-24 16:59:23 -0500

    skjdhfkjs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,9 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->name);
-		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->name = name_dup;
