81e268275aeaa3508e3d03f604fb9cb0da744631
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-24 17:49:36 +0000

    maybe i am getting this?

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+* print_numbers - a function that prints numbers
+* Description - see above
+* @separator: what is printed inbetween the numbers
+* @n: the number of integers passed to the funtion
+* Return: Always 0 (success)
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list(ap);
+
+	va_start(ap, n);
+
+	printf("%s", va_arg(ap, int));
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%s%s", separator, va_arg(ap, int));
+		}
+	}
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf(" %s", va_arg(ap, int));
+		}
+	}
+
+	putchar("\n");
+}
