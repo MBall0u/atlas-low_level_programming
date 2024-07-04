5bebd0b532256ef4e22707696b64f67011b3249d
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-04 17:10:19 -0500

    M - 5:04 PM - made file free_listint

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - frees allocated memory for the nodes
+ * Description - goes node by node freeing the dynamically
+ * allocated memory until everthing is free'd
+ * @head: the head node passed as an argument
+ * Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+		free(temp);
+	}
+}
