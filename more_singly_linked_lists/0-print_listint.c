94f6521145c397a26ac9a6e0b1032e921b62dd8e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-04 15:30:44 -0500

    M - 3:30 PM - made print_listint

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint - prints the elements of the current node
+ * Description - starts at the head node and prints the
+ * current nodes element and then goes to the next node
+ * @h: the head node that is passed to the function
+ * Return: node count (success)
+*/
+size_t print_listint(const listint_t *h)
+{
+	int n_count = 0; /* node counter */
+
+	while (h != NULL)
+	{
+		if (h->n != NULL)
+		{
+			printf("%d\n", h->n);
+		}
+		n_count++;
+		h = h->next;
+	}
+	return (n_count);
+}
