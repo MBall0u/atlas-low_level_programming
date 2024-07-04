84fe6186aa557451b0a57d0643f9c290edeeb87e
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-04 01:22:51 -0500

    M - 1:22 AM - made the add_node function

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node to the singly linked list
+ * Description - adds a new node, copies the string,
+ * calculates the string length and adds that to the
+ * node as well
+ * @head: the head node passed to the function
+ * @str: the string passed to the function as an argument
+ * Return: 0 (success)
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	list_t *new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+}
+/**
+ * _strlen - counts the amount of characters in a string
+ * Description - counts the number of characters that are in
+ * a string and returns the count to the calling function
+ * @s: provided string passed as argument
+ * Return: count (success)
+*/
+int _strlen(char *s)
+{
+	int count = 0; /* character count */
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
