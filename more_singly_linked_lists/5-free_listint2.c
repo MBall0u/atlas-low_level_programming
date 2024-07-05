b62141fc26cb34d47146f9d281a74e89a6488e37
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-05 14:44:26 +0000

    M - 9:44 AM - made file free_listint2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees dynamically allocated memory
+ * Description - starts at the head node and free's dynamically allocated memory
+ * node by node and then sets the head node to NULLL
+ * @head: the head node passed as an argument
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
