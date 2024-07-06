29a90d17e7ba6f896de75c47b9e6411ef4655c9c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-06 18:37:33 +0000

    M - 1:36 PM - made files README, binary_to_uint, and main.h and added the prototypes to it

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to base 10
+ * Description - takes an array of binary numbers
+ * and returns the base 10 number
+ * @b: the binary string
+ * Return: the resulting base 10 number
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int count = 0, power, pow_count, pos = 0;
+
+	if (b == NULL)
+		return (NULL);
+	while (b[count] != '\0')
+		count++;
+	count--;
+	while (count >= 0)
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+		power = 1;
+		if (b[count] == '1')
+		{
+			for (pow_count = pos; pow_count > 0; pow_count--)
+				power *= 2;
+		}
+		res += (power * ((b[count]) - '0'));
+		count--;
+		pos++;
+	}
+	return (res);
+}
---
+++ bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This directory is being used for the project C - Bit manipulation
