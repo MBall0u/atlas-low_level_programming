0df0768cc1f0f501ed62b2d4863e8939f806e956
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-08 23:17:52 -0500

    M - 11:17 PM - made the file print_binary

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_binary - prints a decimal as binary
+ * Description - Takes in a decimal and then uses
+ * bitwise functions to print out the decimal in binary
+ * @n: the inputted decimal
+ * Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 1;
+
+	while (i < n)
+	{
+		i <<= 1;
+		if (i >= n)
+		{
+			i >>= 1;
+			break;
+		}
+	}
+
+	while (i > 0)
+	{
+		if (n >= i)
+		{
+			_putchar('1');
+			n -= i;
+		}
+		else
+		{
+			_putchar('0');
+		}
+
+		i >>= 1;
+	}
+}
