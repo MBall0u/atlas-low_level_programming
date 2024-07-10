bd981132ef9163cdeeccf114d2c63c6622928494
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-10 17:32:23 +0000

    M - 12:32 PM - made the file print_dlistint

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* print_dlistint - prints all elements in a linked list
+* Description - starts at the head node passed to the function and
+* iterates through the doubly linked list and prints the elements
+* @h: the head node passed to the function
+* Return: amount of elements
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+	return(counter);
+}
