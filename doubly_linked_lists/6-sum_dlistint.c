25a585374f20ade0632a681f5916df186cccae88
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-10 18:27:14 -0500

    M - 6:25 PM - made the file sum_dlistint

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_dlistint - adds all of the elements in the list together
+ * Description - starts at the head node and iterates through the
+ * doubly linked list to get the sum and return the total
+ * @head: head node passed to function
+ * Return: sum total
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int res = 0;
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+		return (0);
+	
+	while (temp != NULL)
+	{
+		res += temp->n;
+		temp = temp->next;
+	}
+	return (res);
+}
