1c60d0106389ef5d29263a04032f6016bac064f1
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 23:44:09 +0000

    M - 6:43 PM - betty corrections for hash_table_create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,13 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-	
+
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
-	
+
 	return (table);
 }
