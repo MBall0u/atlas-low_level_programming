a9fe186f82453bfeec9d2d288ba181a531a4d1d0
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-16 03:25:45 +0000

    betty correction

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,11 +17,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht)
 		return (0);
-	
+
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
-	
+
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
