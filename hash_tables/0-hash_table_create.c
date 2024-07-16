5c5636f2485c572efb29d26b1d85471701d7c712
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 20:51:03 -0500

    M - 8:50 PM - test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,11 +13,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
 	table->array = malloc(sizeof(hash_node_t *) * size);
+	if(!table->array)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
+	{
 		table->array[i] = NULL;
+		table->size[i] = NULL;
+	}
 
 	return (table);
 }
