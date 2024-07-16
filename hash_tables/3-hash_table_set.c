5cc8acf558f7525c64e8422573d91d4afa26baf8
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 23:08:04 -0500

    kjsdfnjks

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	new->next = NULL;
 
-	checker = ht->array[index]
+	checker = ht->array[index];
 
 	if (checker == NULL)
 	{
