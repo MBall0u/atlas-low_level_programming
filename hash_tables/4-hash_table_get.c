c3ee78392b2cc036b9ebe975faa5c5991704c848
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 23:52:08 -0500

    ksdfksdbfk

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t **array, *temp;
 
+	if (!ht)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	array = ht->array;
 
