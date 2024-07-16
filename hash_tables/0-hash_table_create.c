81a001362df58ffbb7551d639cbddd06c92668b6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 19:23:56 -0500

    M - 7:23 PM - fixed return and an if check

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,11 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!table->array)
-		return (NULL);
 
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
-	return (&table);
+	return (table);
 }
