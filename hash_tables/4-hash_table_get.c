801f76445584f50073750bc321dc053d9ff8465a
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-15 23:49:42 -0500

    M - 11:49 PM - made hash_table_get

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value
+ * Description - takes an inputted hash table and a key
+ * and finds the value associated with it
+ * @ht: inputted hash table
+ * @key: the passed key
+ * Return: the value of the key
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t **array, *temp;
+
+	index = key_index(key, ht->size);
+	array = ht->array;
+
+	temp = array[index];
+	while (temp)
+	{
+		if (strcmp(key, temp->key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
