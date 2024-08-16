e30c62a9d136e4579f8a1da4b71a66ed0a157654
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-16 18:52:37 -0500

    M - test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -45,6 +45,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp = checker;
 			checker = temp->next;
 		}
+		temp = ht->array[index];
 		new->next = temp;
 		ht->array[index] = new;
 	}
