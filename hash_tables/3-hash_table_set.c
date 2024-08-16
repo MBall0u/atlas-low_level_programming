fe1c17a9b5bb9fe83045ec3404617c4fd98168fb
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-16 18:38:00 -0500

    M - 6:37 PM - sfkjsnd

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			checker = temp->next;
 		}
 		new->next = temp;
-		checker = new;
+		ht->array[index] = new;
 	}
 	return (1);
 }
