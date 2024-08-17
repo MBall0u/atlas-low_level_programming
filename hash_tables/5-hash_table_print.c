ba897f1c5da592baafed8d55ee13bfeab771ef80
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-16 23:47:32 -0500

    M - test

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - see below
+ * Description - takes a hash table and prints a formatted
+ * output
+ * @ht: the hash table
+ * Return: Void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	int index, comma = 0;
+	hash_node_t *temp;
+
+	printf("{");
+	if (ht)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			if (ht->array[index])
+			{
+				if (comma != 0)
+					printf(", ");
+				temp = ht->array[index];
+				printf("'%s': '%s'", temp->key, temp->value);
+				comma++;
+			}
+		}
+	}
+	printf("}");
+	putchar('\n');
+}
